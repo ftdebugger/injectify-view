f9c75b7a14a156afd78e4eb91cc7438e24e27db3
Evgeny Shpilevsky <evgeny@shpilevsky.com>
2014-07-07 01:12:00 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+(function () {
+    //noinspection BadExpressionStatementJS
+    "use strict";
+
+    var Handlebars = require("injectify/runtime"),
+        regionHelper = require("regions-extra");
+
+
+    Handlebars.registerHelper("view", function (View, name, options) {
+        if (typeof name == "object") {
+            options = name;
+            name = _.uniqueId('view');
+        }
+
+        var view = this.view;
+
+        if (view) {
+            view.on('render', function () {
+                view[name].show(new View(options.hash));
+            });
+        }
+        else {
+            console.warn("Cannot find 'view' for handlebars view helper '" + name + "'");
+        }
+
+        return regionHelper.call(this, name, options);
+    });
+
+    var mixinTemplateHelpers = Marionette.View.prototype.mixinTemplateHelpers;
+    Marionette.View.prototype.mixinTemplateHelpers = function (data) {
+        data = mixinTemplateHelpers.call(this, data);
+        data.view = this;
+
+        return data;
+    };
+
+})();
