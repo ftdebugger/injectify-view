6883793544ada8d3cd6412eb54a24bc87d30d805
Evgeny Shpilevsky <evgeny@shpilevsky.com>
2014-12-30 13:27:35 +0000

    Backporting from 1.0.x

--- index.js
+++ index.js
@@ -8,6 +8,16 @@
 
     require("regions-extras/handlebars").setInstance(Handlebars);
 
+    var getView = function (context) {
+        if (context) {
+            while (context && !context.view && context.__parent__) {
+                context = context.__parent__;
+            }
+
+            return context ? context.view : null;
+        }
+    };
+
     /**
      * @param {Backbone.View} View
      * @param {String} [name]
@@ -44,13 +54,10 @@
             view = hash.view;
         }
         else {
-            var context = this;
-
-            while (context && !context.view && context.__parent__) {
-                context = context.__parent__;
+            view = getView(this);
+            if (!view && options.data && options.data._parent) {
+                view = getView(options.data._parent.root)
             }
-
-            view = context ? context.view : null;
         }
 
         if (options.fn) {
@@ -62,15 +69,32 @@
         }
 
         if (view) {
-            view.on('render', function () {
-                view[name].show(new View(hash));
-            });
+            var onRender = function () {
+                if (!view[name]) {
+                    console.error('Region is not initialized, may be view is destroyed');
+                }
+                else {
+                    view[name].show(new View(hash));
+                }
+            };
+
+            var onDestroy = function () {
+                view.off('render', onRender);
+            };
+
+            view.once('render', onRender);
+            view.once('destroy', onDestroy);
         }
         else {
             console.warn("Cannot find 'view' for handlebars view helper '" + name + "'");
         }
 
-        return regionHelper.call(this, name, options);
+        var regionHash = _.clone(hash);
+        regionHash.view = view;
+
+        return regionHelper.call(this, name, {
+            hash: regionHash
+        });
     };
 
     var mixinTemplateHelpers = Marionette.View.prototype.mixinTemplateHelpers;
